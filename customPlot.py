b7574080bf9daf3a373cca910bd3ebc8600a3e78
MengyuWu <ayu@marin.colorado.edu>
2021-10-26 21:52:11 -0600

    New function added
    New function to convert cpt to cmap:
    New fucntions addAxes, addCAxes and moveAxes

--- customPlot.py
+++ customPlot.py
@@ -1,20 +1,21 @@
+import os
 import numpy as np
 import cartopy.crs as ccrs
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 from geographiclib.geodesic import Geodesic
-from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.colors import LinearSegmentedColormap, ListedColormap
 
 labelFont = {'weight':'normal','size':14}
 titleFont = {'weight':'bold','size':16}
 
-import os,pycpt;
-cvcpt = pycpt.load.gmtColormap(os.path.dirname(__file__)+'/cv_original.cpt')
-rbcpt = pycpt.load.gmtColormap(os.path.dirname(__file__)+'/rainbow.cpt').reversed()
+# import os,pycpt;
+# cvcpt = pycpt.load.gmtColormap(os.path.dirname(__file__)+'/cv_original.cpt')
+# rbcpt = pycpt.load.gmtColormap(os.path.dirname(__file__)+'/rainbow.cpt').reversed()
 
 coastlineData = '/home/ayu/SubdInv/models/ne_10m_coastline/ne_10m_coastline'
-physioData = os.path.dirname(__file__)+'/physio/physio'
+physioData = os.path.dirname(__file__)+'/physio/physio' #https://water.usgs.gov/GIS/dsdl/physio_shp.zip
 
 def plotLocalBase(minlon, maxlon, minlat, maxlat,resolution='c',coastline=None,figwidth=None,ax=None,
              dlat=5.0, dlon=5.0, topo=None):
@@ -77,6 +78,15 @@ def plotGlobalBase(figwidth=None,ax=None,resolution='l'):
     m.drawcoastlines(linewidth=1)
     return fig,m
 
+def plotLocalCart(minlon, maxlon, minlat, maxlat):
+    crsPlate = ccrs.PlateCarree()
+    crs = ccrs.PlateCarree()
+    plt.figure()
+    ax = plt.axes(projection=crs)
+    ax.set_extent((minlon, maxlon, minlat, maxlat))
+    ax.coastlines()
+    return ax,crsPlate
+
 def plotGlobalCart(type='Plate',**kwargs):
     crsPlate = ccrs.PlateCarree()
     if type == 'Plate':
@@ -101,19 +111,13 @@ def colorCycle():
     return cl
 
 def cpt2cmap(cptfile,name='NewColorMap',N=256):
-    ''' Generate linearly changed colormap from cpt file, B,F,N lines are ignored 
-    Attention: when converted into cmap, the color palette table will be resampled. It is ok
-    when cpt is continuous, however there may be a color jump sometime, like sea level when plot
-    topo graph. The cmap struct will have N color ranges with N+1 boundaries. The color is
-    resampled at np.linspace(0,1,N) and boundaries are np.linspace(0,1,N+1), after normalizing 
-    z-value in cpt to [0,1]. Each color range is half-closed interval [a,b) except last one, which
-    is closed interval [a,b].
-    So if you want to get the color jump correctly, you can choose appropriate N here to make sure
-    make sure one of the range boundaries locates at normalized jump point of cpt file. And then
-    choose vmin and vmax when plotting to make sure the value for plot is also at that point. For example:
-    if cpt is for topo plot, min = -5000, max = 8000, jump at z = 0
-    N = 260, vmax/(-vmin) = 8/5 would be a good set of parameters.
-    '''
+    def interpColorList(listedCmap,N):
+        R = np.linspace(listedCmap[0][0],listedCmap[1][0],N)
+        G = np.linspace(listedCmap[0][1],listedCmap[1][1],N)
+        B = np.linspace(listedCmap[0][2],listedCmap[1][2],N)
+        A = np.linspace(listedCmap[0][3],listedCmap[1][3],N)
+        return np.array([R,G,B,A]).T
+
     cptmat = []
     with open(cptfile) as f:
         for line in f:
@@ -122,56 +126,27 @@ def cpt2cmap(cptfile,name='NewColorMap',N=256):
                 continue
             else:
                 cptmat.append([float(i) for i in line.split()])
-
-    zcheckb = np.array([cptmat[i][0] for i in range(len(cptmat))])
-    zchecke = np.array([cptmat[i][4] for i in range(len(cptmat))])
-    if np.any(zchecke - zcheckb < 0) or np.any(zchecke[:-1] - zcheckb[1:] != 0):
-        raise ValueError('Illegal .cpt file! The z-values should increase monotonically.')
-
-    red     = []
-    blue    = []
-    green   = []
-    alpha   = []
-    for i in range(len(cptmat)):
-        zvalue = (cptmat[i][0]-cptmat[0][0])/(cptmat[-1][0]-cptmat[0][0])
-        if i == 0:
-            red.append(     (zvalue,0,cptmat[i][1]/255.) )
-            green.append(   (zvalue,0,cptmat[i][2]/255.) )
-            blue.append(    (zvalue,0,cptmat[i][3]/255.) )
-            alpha.append(   (zvalue,1.0,1.0) )
-        else:
-            red.append(     (zvalue,cptmat[i-1][5]/255.,cptmat[i][1]/255.) )
-            green.append(   (zvalue,cptmat[i-1][6]/255.,cptmat[i][2]/255.) )
-            blue.append(    (zvalue,cptmat[i-1][7]/255.,cptmat[i][3]/255.) )
-            alpha.append(   (zvalue,1.0,1.0) )
-    red.append(     (zvalue,cptmat[i][5]/255.,1.0) )
-    green.append(   (zvalue,cptmat[i][6]/255.,1.0) )
-    blue.append(    (zvalue,cptmat[i][7]/255.,1.0) )
-    alpha.append(   (zvalue,1.0,1.0) )
-    cdict = {'red':red, 'green':green, 'blue':blue, 'alpha':alpha}
-    return LinearSegmentedColormap(name,cdict,N)
-
-def mkcmap(inM,name='NewColorMap',N=256):
-    ''' 
-    Generate linearly changed colormap, the larger N, the smoother the change would be.
-    inM is a list of [z-value, R, G, B, (alpha)]
-    '''
-    inM = np.array(inM)
-    n = inM.shape[0]
-    if inM.shape[1] == 4:
-        inM = np.append(inM,np.ones([n,1]),axis=1)
-    inM[:,0] = (inM[:,0] - inM[0,0]) / (inM[-1,0] - inM[0,0])
-    red     = []
-    blue    = []
-    green   = []
-    alpha   = []
-    for i in range(n):
-        red.append((    inM[i,0],inM[i,1],inM[i,1]))
-        green.append((  inM[i,0],inM[i,2],inM[i,2]))
-        blue.append((   inM[i,0],inM[i,3],inM[i,3]))
-        alpha.append((  inM[i,0],inM[i,4],inM[i,4]))
-    cdict = {'red':red, 'green':green, 'blue':blue, 'alpha':alpha}
-    return LinearSegmentedColormap(name,cdict,N)
+    cptmat = np.array(cptmat)
+    zLs = cptmat[:,4] - cptmat[:,0]
+    Ns = np.round(zLs/zLs.sum()*N)
+    Ns[-1] += N-Ns.sum()
+    colorList = np.zeros((N,4))
+    normX = np.zeros(len(zLs)+1)
+    normY = np.zeros(len(zLs)+1)
+    count = 0
+    for i in range(len(zLs)):
+        colorList[count:count+int(Ns[i])] = interpColorList(
+                                    [[cptmat[i,1]/255,cptmat[i,2]/255,cptmat[i,3]/255,1],
+                                     [cptmat[i,5]/255,cptmat[i,6]/255,cptmat[i,7]/255,1]],
+                                    N=int(Ns[i]))
+        normX[i] = cptmat[i,0]
+        normY[i] = Ns[:i].sum()/N
+        count += int(Ns[i])
+    normX[-1] = cptmat[-1,4]
+    normY[-1] = 1
+    cmap = ListedColormap(colorList)
+    norm = CustomNorm(normX,normY)
+    return cmap,norm
 
 class CustomNorm(mpl.colors.Normalize):
     def __init__(self,x,y,clip=False):
@@ -183,17 +158,100 @@ class CustomNorm(mpl.colors.Normalize):
         else:
             return np.ma.masked_array(np.interp(value,self.x,self.y))
 
+def addAxes(loc,hasTitle=True):
+    ''' loc = [x,y,w,h] '''
+    if hasTitle:
+        border = [0.125,0.11,0.1,0.12]
+    else:
+        border = [0.125,0.11,0.1,0.08]
+    x,y,w,h = loc
+    dx1,dy1,dx2,dy2 = border
+    rect = [x+w*dx1, y+h*dy1, w*(1-dx1-dx2), h*(1-dy1-dy2)]
+    return plt.axes(rect)
+
+def addCAxes(ax,location='right',size=0.05,pad=0.12):
+    bbox = ax.get_position()
+    x,y,w,h = bbox.x0,bbox.y0,bbox.width,bbox.height
+    if location == 'right':
+        pad = 0.05 if pad is None else pad
+        rect = [x+w+w*pad,y,w*size,h]
+    elif location == 'bottom':
+        pad = 0.12 if pad is None else pad
+        rect = [x,y-w*pad-w*size,w,h*size]
+    else:
+        raise ValueError('Not ready yet')
+    return plt.axes(rect)
+
+def moveAxes(ax,move):
+    bbox = ax.get_position()
+    x,y,w,h = bbox.x0,bbox.y0,bbox.width,bbox.height
+    if move[0] == 'R':
+        x = x+float(move[1:])
+    elif move[0] == 'L':
+        x = x-float(move[1:])
+    elif move[0] == 'U':
+        y = y+float(move[1:])
+    elif move[0] == 'D':
+        y = y-float(move[1:])
+    else:
+        raise ValueError('Wrong move command, should start with U/D/R/L')
+    ax.set_position([x,y,w,h])
 
 # fig, axes = plt.subplots(2,3,figsize=[12,8])
 # plt.subplots_adjust(wspace=0.25,hspace=0.3,left=0.08,right=0.92,bottom=0.08)
 
+cvcpt = cpt2cmap(os.path.dirname(__file__)+'/cv_original.cpt','cv')[0]
+rbcpt = cpt2cmap(os.path.dirname(__file__)+'/rainbow.cpt','rainbow')[0].reversed()
 
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
     plt.ion()
-    print('Testing ...')
-    plotLocalBase(-172.0, -122.0, 52.0, 72.0,resolution='l')
-    fig,m = plotLocalBase(-130,-110,30,50,resolution='l')
-    # fig,m = plotLocalBase(-130,-110,30,50,coastline=coastlineData,topo='True')
-    # plotGlobalBase()
+    # print('Testing ...')
+    # plotLocalBase(-172.0, -122.0, 52.0, 72.0,resolution='l')
+    # fig,m = plotLocalBase(-130,-110,30,50,resolution='l')
+    # # fig,m = plotLocalBase(-130,-110,30,50,coastline=coastlineData,topo=True)
+    # fig,m = plotGlobalBase()
+    # ax,crsPlate = plotGlobalCart()
+    # ax,crsPlate = plotLocalCart(-130,-110,30,50)
+
+
+    # plt.figure()
+    # addAxes([0,0,1/2,1])
+    # addAxes([1/2,0,1/2,1],hasTitle=False)
+
+    # cmap,norm = cpt2cmap('GMT_globe.cpt')
+    # from netCDF4 import Dataset
+    # with Dataset('/home/ayu/Packages/etopo2/ETOPO2v2g_f4.nc') as etopo:
+    # 	lon = etopo.variables['x'][:]
+    # 	lat = etopo.variables['y'][:]
+    # 	topo = etopo.variables['z'][:]
+    # lat_bnd, lon_bnd = [52, 72], [-172, -122]
+    # lat_ind = np.where((lat > lat_bnd[0]) & (lat < lat_bnd[1]))
+    # lon_ind = np.where((lon > lon_bnd[0]) | (lon < lon_bnd[1]))
+    # topo = topo[lat_ind[0],:]
+    # topo = topo[:,lon_ind[0]]
+    # lon  = lon[lon_ind]
+    # lat  = lat[lat_ind]
+    # XX,YY = np.meshgrid(lon,lat)
+    # fig,m = plotLocalBase(-172.0, -122.0, 52.0, 72.0,resolution='l')
+    # m.pcolormesh(XX, YY, topo, shading='gouraud',cmap=cmap,norm=norm,latlon=True)
+
+    # from mpl_toolkits.axes_grid1 import make_axes_locatable
+    # plt.figure()
+    # ax = plt.subplot()
+    # im = ax.imshow(np.arange(100).reshape((10, 10)))
+    # # create an axes on the right side of ax. The width of cax will be 5%
+    # # of ax and the padding between cax and ax will be fixed at 0.05 inch.
+    # divider = make_axes_locatable(ax)
+    # cax = divider.append_axes("right", size="5%", pad=0.50)
+    # plt.colorbar(im, cax=cax)
+    # # plt.colorbar(orientation='horizontal',fraction=0.1,aspect=50,pad=0.08)
+
+
+    # plt.figure()
+    # ax = addAxes([0,0,1,1])
+    # moveAxes(ax,'U0.06')
+    # im = ax.imshow(np.arange(100).reshape((10, 10)))
+    # cax = addCAxes(ax,'bottom')
+    # plt.colorbar(im,cax=cax,orientation="horizontal")
